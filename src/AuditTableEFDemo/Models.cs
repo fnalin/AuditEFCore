a4e70e17c96b0b3bee5107dcaba7742f212053c1
Fabiano Nalin <fabiano.nalin@gmail.com>
2019-02-13 17:57:44 -0200

    Add project

---
+++ src/AuditTableEFDemo/Models.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AuditTableEFDemo
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class Audit
+    {
+        public int Id { get; set; }
+        public string TableName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string KeyValues { get; set; }
+        public string OldValues { get; set; }
+        public string NewValues { get; set; }
+    }
+
+    public class AuditEntry
+    {
+        public AuditEntry(EntityEntry entry)
+        {
+            Entry = entry;
+        }
+
+        public EntityEntry Entry { get; }
+        public string TableName { get; set; }
+        public Dictionary<string, object> KeyValues { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> OldValues { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> NewValues { get; } = new Dictionary<string, object>();
+        public List<PropertyEntry> TemporaryProperties { get; } = new List<PropertyEntry>();
+
+        public bool HasTemporaryProperties => TemporaryProperties.Any();
+
+        public Audit ToAudit()
+        {
+            var audit = new Audit();
+            audit.TableName = TableName;
+            audit.DateTime = DateTime.UtcNow;
+            audit.KeyValues = JsonConvert.SerializeObject(KeyValues);
+            audit.OldValues = OldValues.Count == 0 ? null : JsonConvert.SerializeObject(OldValues);
+            audit.NewValues = NewValues.Count == 0 ? null : JsonConvert.SerializeObject(NewValues);
+            return audit;
+        }
+    }
+}
---
+++ src/AuditTableEFDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AuditTableEFDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DeleteCustomers().Wait();
+            //EditCustomer().Wait();
+            //AddCustomer().Wait();
+            Console.WriteLine("Fim");
+            Console.ReadLine();
+        }
+
+        private static async Task AddCustomer()
+        {
+            var customer1 = new Customer() { FirstName = "Fabiano", LastName = "Alberto" };
+            var customer2 = new Customer() { FirstName = "Priscila", LastName = "Mitui" };
+
+            using (var ctx = new SampleContext())
+            {
+
+                ctx.Customers.Add(customer1);
+                ctx.Customers.Add(customer2);
+
+                await ctx.SaveChangesAsync();
+
+            }
+        }
+
+        private static async Task EditCustomer()
+        {
+
+            using (var ctx = new SampleContext())
+            {
+
+                var c1 = ctx.Customers.Find(1);
+                c1.FirstName = "Change";
+                c1.LastName = "Silva";
+
+                await ctx.SaveChangesAsync();
+
+            }
+        }
+
+        private static async Task DeleteCustomers()
+        {
+
+            using (var ctx = new SampleContext())
+            {
+
+                var c1 = ctx.Customers.Find(1);
+                var c2 = ctx.Customers.Find(2);
+
+                ctx.Customers.RemoveRange(c1, c2);
+
+
+                await ctx.SaveChangesAsync();
+
+            }
+        }
+    }
+}
